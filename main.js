a8b91ee0f2fad7fde5287f820779f729df8c58ee
Nelson Choque <nelson.choque@paku.pe>
2025-01-29 19:15:42 -0500

    update project

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+window.addEventListener("load", () => {
+  const audio = document.getElementById("myAudio");
+  audio.muted = false;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.createElement("audio");
+  audio.src = "audio.mp3";
+  audio.autoplay = true;
+  document.body.appendChild(audio);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#e7d239",
+      },
+    },
+  },
+  plugins: [],
+};
